2d1845b241e00020286171b289347ec8cdbfcd28
Hugo de Freitas <hugo.encaminhados@gmail.com>
2024-06-19 11:53:37 -0300

    Algumas mudanças

--- simuladores-sva/src/app/components/button.tsx
+++ simuladores-sva/src/app/components/button.tsx
@@ -1,10 +1,17 @@
 import React, { useState } from "react";
+import { useRouter } from 'next/navigation';
 
 interface ButtonsProps {
     text: string;
 }
 
 const Buttons: React.FC<ButtonsProps> = (props: ButtonsProps) => {
+    const router = useRouter();
+    // function catchClick(){
+    //     handleClick();
+    //     redirectToProducts();
+    // }
+
     const [isClicked, setIsClicked] = useState(false);
 
     const handleClick = () => {
@@ -12,10 +19,11 @@ const Buttons: React.FC<ButtonsProps> = (props: ButtonsProps) => {
         setTimeout(() => {
             setIsClicked(false)
         }, 100);
+        // router.push('../proximaEtapa');
     };
 
     return (
-        <button onClick={handleClick} className={`w-[40%] lg:w-[20%] self-center bg-[#C9D200] hover:bg-[#DBE202] text-white font-bold py-2 px-4 border-[#8C9100] hover:border-[#7D8201] rounded ${isClicked ? 'border-b-0 mt-10' : 'mt-9 border-b-4'}`}>{props.text}</button>
+        <button id="btn" onClick={handleClick} className={`self-center w-[40%] lg:w-[20%] bg-[#C9D200] hover:bg-[#DBE202] text-white font-bold py-2 px-4 border-[#8C9100] hover:border-[#7D8201] rounded ${isClicked ? 'border-b-0 mt-10' : 'mt-9 border-b-4'}`}>{props.text}</button>
     )
 }
 
---
+++ simuladores-sva/src/app/proximaEtapa/proximaEtapa.tsx
@@ -0,0 +1,14 @@
+'use client';
+
+import React from 'react';
+
+const ProximaEtapa: React.FC = () => {
+
+  return (
+    <div className="p-8">
+      <h1 className="text-2xl">Dados da Simulação</h1>
+    </div>
+  );
+};
+
+export default ProximaEtapa;
